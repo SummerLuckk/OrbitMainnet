b636000436e286540c37fff5dbbc1c30d428535f
SummerLuckk <summerluckyalrish@gmail.com>
2024-10-12 15:29:05 +0530

    fetching balance of wallet and displaying it

--- src/app/Components/pages/userHome/MainComponent.tsx
+++ src/app/Components/pages/userHome/MainComponent.tsx
@@ -1,143 +1,167 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react'
-import { ChevronLeft, ChevronRight, Copy, Grid, Home, Layers, PlusCircle, Settings, Share2 } from 'lucide-react'
+import { useEffect, useState } from 'react';
+import { ChevronLeft, Copy, Home, PlusCircle, Settings } from 'lucide-react';
 import Blockies from "react-blockies";
-import Dashboard from './Dashboard'
-import NewTransaction from './NewTransaction'
-import WalletSettings from './WalletSettings'
-import Link from 'next/link'
-import { useParams } from 'next/navigation'
-import { truncateAddress } from '@/app/utils/truncateAddress'
+import Dashboard from './Dashboard';
+import NewTransaction from './NewTransaction';
+import WalletSettings from './WalletSettings';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
+import { truncateAddress } from '@/app/utils/truncateAddress';
+import { Address, createPublicClient, formatUnits, http } from 'viem';
 
-type MenuItem = 'Dashboard' | 'New Transaction' | 'Settings'
 
-const menuItems: MenuItem[] = ['Dashboard', 'New Transaction', 'Settings']
-type SignerWithName = {
-    name: string;
-    address: string;
+const bittorrentchainTestnet = {
+    id: 1029,
+    name: "BitTorrent Chain Donau",
+    nativeCurrency: {
+        decimals: 18,
+        name: "BitTorrent Chain Donau",
+        symbol: "BTT",
+    },
+    rpcUrls: {
+        default: { http: ["https://pre-rpc.bittorrentchain.io/"] },
+    },
 };
 
+const publicClient = createPublicClient({
+    chain: bittorrentchainTestnet,
+    transport: http("https://pre-rpc.bittorrentchain.io/"),
+});
+
+type MenuItem = 'Dashboard' | 'New Transaction' | 'Settings';
+type SignerWithName = { name: string; address: string; };
 type Wallet = {
     _id: string;
     walletAddress: string;
     name: string;
-    signerAddresses: string[]; // Array of signer addresses
-    signerWithName: SignerWithName[]; // Array of objects with name and address
+    signerAddresses: string[];
+    signerWithName: SignerWithName[];
     createdBy: string;
-    createdAt: string; // Could also be Date, but you're currently receiving it as a string
-    requiredSignatures: string; // Consider changing to number if the value will always be numeric
+    createdAt: string;
+    requiredSignatures: string;
 };
 
 export default function MainComponent() {
-    const [activeMenuItem, setActiveMenuItem] = useState<MenuItem>('Dashboard')
-    const [walletDetails, setWalletDetails] = useState<Wallet | null>()
+    const [activeMenuItem, setActiveMenuItem] = useState<MenuItem>('Dashboard');
+    const [walletDetails, setWalletDetails] = useState<Wallet | null>(null);
+    const [balance, setBalance] = useState<string | null>(null);
     const params = useParams();
-    console.log("params", params)
     const walletAddress = params.address;
+
     useEffect(() => {
         const fetchWallets = async () => {
-
             try {
                 const response = await fetch(`/api/wallet/get-single-wallet-details?walletAddress=${walletAddress}`);
                 const data = await response.json();
 
                 if (response.ok) {
-                    console.log(data)
                     setWalletDetails(data.wallet.length > 0 ? data.wallet[0] : null);
                 }
             } catch (err) {
                 console.log('An error occurred while fetching wallets');
             }
         };
-        if (walletAddress) { fetchWallets(); }
+
+        const fetchBalance = async () => {
+            if (walletAddress) {
+                try {
+                    const balanceData = await publicClient.getBalance({ address: walletAddress as Address});
+                    setBalance(formatUnits(balanceData, 18));
+                } catch (err) {
+                    console.error('Error fetching balance:', err);
+                    setBalance(null);
+                }
+            }
+        };
+
+        if (walletAddress) {
+            fetchWallets();
+            fetchBalance();
+        }
     }, [walletAddress]);
+
     const renderContent = () => {
         switch (activeMenuItem) {
-        case 'Dashboard':
-            return <Dashboard />
-        case 'New Transaction':
-            return <NewTransaction />
-        case 'Settings':
-            return <WalletSettings />
-        default:
-            return null
+            case 'Dashboard':
+                return <Dashboard />;
+            case 'New Transaction':
+                return <NewTransaction />;
+            case 'Settings':
+                return <WalletSettings />;
+            default:
+                return null;
         }
-    }
+    };
 
     return (
-        <>
-            <div className="flex min-h-screen bg-dark-black text-white font-dmsans">
-                {/* Sidebar */}
-                <div className="w-64 bg-dark-gray ">
-                    <ul className="space-y-2 py-4 px-4 border-b border-border-light">
-
-                        <li >
-                            <Link
-                                href={"/welcome/accounts"}
-                                className="flex w-full items-center text-sm font-bold rounded-lg p-4 bg-border-light"
-                            >
-
-                                <ChevronLeft className="mr-4 w-4 h-4" />
-
-                                Back to Wallets
-                            </Link>
-                        </li>
-                    </ul>
-                    {/* Profile Section */}
-                    <div className="mb-6 px-4 py-4 border-b border-border-light">
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-center">
-                                <Blockies
-                                    seed="Just For Fun"
-                                    size={10}
-                                    scale={5}
-                                    className="mr-3 rounded-full"
-                                />
-                                <div>
-                                    <p className="text-sm font-semibold">{walletDetails ? walletDetails?.name : "Just For Fun"}</p>
-                                    <p className="text-xs text-gray-400">{walletAddress ? truncateAddress(walletAddress.toString()) : ""}</p>
-                                </div>
-                                <div className="ml-4 flex justify-start gap-4">
-                                    <button className="rounded-lg bg-black text-accent p-2">
-                                        <Copy className="h-4 w-4" />
-                                    </button>
-                                </div>
+        <div className="flex min-h-screen bg-dark-black text-white font-dmsans">
+            {/* Sidebar */}
+            <div className="w-64 bg-dark-gray">
+                <ul className="space-y-2 py-4 px-4 border-b border-border-light">
+                    <li>
+                        <Link
+                            href={"/welcome/accounts"}
+                            className="flex w-full items-center text-sm font-bold rounded-lg p-4 bg-border-light"
+                        >
+                            <ChevronLeft className="mr-4 w-4 h-4" />
+                            Back to Wallets
+                        </Link>
+                    </li>
+                </ul>
+                {/* Profile Section */}
+                <div className="mb-6 px-4 py-4 border-b border-border-light">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center">
+                            <Blockies
+                                seed="Just For Fun"
+                                size={10}
+                                scale={5}
+                                className="mr-3 rounded-full"
+                            />
+                            <div>
+                                <p className="text-sm font-semibold">{walletDetails ? walletDetails.name : "Just For Fun"}</p>
+                                <p className="text-xs text-gray-400">{walletAddress ? truncateAddress(walletAddress.toString()) : ""}</p>
+                                <p className="text-xs text-gray-400">Balance: {balance !== null ? balance : "Loading..."}</p>
+                            </div>
+                            <div className="ml-4 flex justify-start gap-4">
+                                <button className="rounded-lg bg-black text-accent p-2">
+                                    <Copy className="h-4 w-4" />
+                                </button>
                             </div>
                         </div>
-
-                        <button className="mt-6 mb-4 w-full rounded-lg bg-accent py-2 text-center font-semibold text-black"
-                            onClick={() => setActiveMenuItem('New Transaction')}>
-                            New transaction
-                        </button>
                     </div>
-                    <nav className='px-4'>
-                        <ul className="space-y-2">
-                            {menuItems.map((item) => (
-                                <li key={item}>
-                                    <button
-                                        className={`flex w-full items-center text-sm font-bold rounded-lg p-4 ${activeMenuItem === item ? 'bg-border-light' : 'hover:bg-accent-light'
-                                            }`}
-                                        onClick={() => setActiveMenuItem(item)}
-                                    >
-                                        {item === 'Dashboard' && <Home className="mr-4 w-4 h-4" />}
-                                        {item === 'New Transaction' && <PlusCircle className="mr-4 w-4 h-4" />}
-                                        {item === 'Settings' && <Settings className="mr-4 w-4 h-4" />}
-                                        {item}
-                                    </button>
-                                </li>
-                            ))}
-                        </ul>
-                    </nav>
-                </div>
 
-                {/* Main content */}
-                <div className="flex-1 p-8">
-                    <h2 className="mb-4 text-2xl font-semibold">{activeMenuItem}</h2>
-                    {renderContent()}
+                    <button className="mt-6 mb-4 w-full rounded-lg bg-accent py-2 text-center font-semibold text-black"
+                        onClick={() => setActiveMenuItem('New Transaction')}>
+                        New transaction
+                    </button>
                 </div>
+                <nav className='px-4'>
+                    <ul className="space-y-2">
+                        {['Dashboard', 'New Transaction', 'Settings'].map(item => (
+                            <li key={item}>
+                                <button
+                                    className={`flex w-full items-center text-sm font-bold rounded-lg p-4 ${activeMenuItem === item ? 'bg-border-light' : 'hover:bg-accent-light'}`}
+                                    onClick={() => setActiveMenuItem(item)}
+                                >
+                                    {item === 'Dashboard' && <Home className="mr-4 w-4 h-4" />}
+                                    {item === 'New Transaction' && <PlusCircle className="mr-4 w-4 h-4" />}
+                                    {item === 'Settings' && <Settings className="mr-4 w-4 h-4" />}
+                                    {item}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
             </div>
-        </>
-    )
-}
 
+            {/* Main content */}
+            <div className="flex-1 p-8">
+                <h2 className="mb-4 text-2xl font-semibold">{activeMenuItem}</h2>
+                {renderContent()}
+            </div>
+        </div>
+    );
+}
--- src/app/welcome/accounts/page.tsx
+++ src/app/welcome/accounts/page.tsx
@@ -1,27 +1,50 @@
-
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import Blockies from "react-blockies";
 import { MoreVertical } from "lucide-react";
 import Link from "next/link";
 import { useAccount } from "wagmi";
 import { MultisigWallet } from "@/app/types/types";
+import { Address, createPublicClient, formatUnits, http } from "viem";
+
+export const bittorrentchainTestnet = {
+    id: 1029,
+    name: "BitTorrent Chain Donau",
+    nativeCurrency: {
+        decimals: 18,
+        name: "BitTorrent Chain Donau",
+        symbol: "BTT",
+    },
+    rpcUrls: {
+        default: { http: ["https://pre-rpc.bittorrentchain.io/"] },
+    },
+    blockExplorers: {
+        default: { name: "schedule-transactions scan", url: "https://testscan.bittorrentchain.io/" },
+    },
+    testnet: true,
+};
+
+const publicClient = createPublicClient({
+    chain: bittorrentchainTestnet,
+    transport: http("https://pre-rpc.bittorrentchain.io/"),
+});
 
 function Accounts() {
     const { address } = useAccount();
-    const [userWallets, setUserWallets] = useState<MultisigWallet[] | []>([])
+    const [userWallets, setUserWallets] = useState<MultisigWallet[]>([]);
+    const [balances, setBalances] = useState<{ [key: string]: string }>({});
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
 
     useEffect(() => {
         const fetchWallets = async () => {
-            setLoading(true)
+            setLoading(true);
             try {
                 const response = await fetch(`/api/wallet/get-by-user?createdBy=${address}`);
                 const data = await response.json();
 
                 if (response.ok) {
-                    console.log(data)
+                    console.log(data);
                     setUserWallets(data.wallets.length > 0 ? data.wallets : []);
                 } else {
                     setError(data.message || 'Failed to fetch wallets');
@@ -32,65 +55,82 @@ function Accounts() {
                 setLoading(false);
             }
         };
+
         if (address) { fetchWallets(); }
     }, [address]);
 
+    useEffect(() => {
+        const fetchBalances = async () => {
+            const newBalances: { [key: string]: string } = {};
 
-    const [hasAccounts, setHasAccounts] = useState(true)
+            for (const wallet of userWallets) {
+                try {
+                    const balance = await publicClient.getBalance({ address: wallet.walletAddress as Address });
+                    newBalances[wallet.walletAddress] = balance.toString(); // Convert balance to string for easier display
+                } catch (err) {
+                    console.error(`Failed to fetch balance for ${wallet.walletAddress}:`, err);
+                    newBalances[wallet.walletAddress] = "Error fetching balance";
+                }
+            }
+
+            setBalances(newBalances);
+        };
+
+        if (userWallets.length > 0) { fetchBalances(); }
+    }, [userWallets]);
+
     return (
-        <>
-            <div className="min-h-screen bg-dark-black text-white p-8 font-dmsans">
-                <div className="max-w-5xl mx-auto">
-                    <div className="flex justify-between items-center mb-8">
-                        <h1 className="text-3xl font-bold">Orbit Multisig Wallets</h1>
-                        <Link href="/new-orbit/create" className="bg-accent text-black font-semibold py-2 px-4 rounded">
-                            Create account
-                        </Link>
-                    </div>
+        <div className="min-h-screen bg-dark-black text-white p-8 font-dmsans">
+            <div className="max-w-5xl mx-auto">
+                <div className="flex justify-between items-center mb-8">
+                    <h1 className="text-3xl font-bold">Orbit Multisig Wallets</h1>
+                    <Link href="/new-orbit/create" className="bg-accent text-black font-semibold py-2 px-4 rounded">
+                        Create account
+                    </Link>
+                </div>
 
-                    <div className="bg-dark-gray rounded-lg p-8 mb-6">
-                        <h2 className="text-md font-semibold mb-4">
-                            My accounts <span className="text-gray-500">{hasAccounts && "(" + userWallets?.length + ")"}</span>
-                        </h2>
-                        {userWallets.length > 0 ? userWallets.map((wallet) => (
-                            <Link href={`/user-home/${wallet.walletAddress}`} className="block border border-border-light rounded-lg p-4 hover:bg-[#ffffff14] cursor-pointer" >
-                                <div className="flex items-center justify-between">
-                                    <div className="flex items-center space-x-3">
-                                        <div className="relative w-10 h-10">
-                                            <Blockies
-                                                className="table-user-gradient rounded-full"
-                                                seed={wallet.walletAddress ? wallet.walletAddress.toString() : "Orbit MultiSig"}
-                                                size={15}
-                                                scale={3}
-
-                                            />
-                                            <div className="absolute top-0 -right-2 bg-accent text-black text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
-                                                {wallet.requiredSignatures}/{wallet.signerAddresses.length}
-                                            </div>
-
-
-                                        </div>
-                                        <div className="pl-4 ">
-                                            <p className="font-medium">{wallet.name}</p>
-                                            <p className="text-gray-500 font-medium">{wallet.walletAddress}</p>
-
+                <div className="bg-dark-gray rounded-lg p-8 mb-6">
+                    <h2 className="text-md font-semibold mb-4">
+                        My accounts <span className="text-gray-500">{userWallets.length > 0 && `(${userWallets.length})`}</span>
+                    </h2>
+                    {userWallets.length > 0 ? userWallets.map((wallet) => (
+                        <Link key={wallet.walletAddress} href={`/user-home/${wallet.walletAddress}`} className="block border border-border-light rounded-lg p-4 hover:bg-[#ffffff14] cursor-pointer">
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-center space-x-3">
+                                    <div className="relative w-10 h-10">
+                                        <Blockies
+                                            className="table-user-gradient rounded-full"
+                                            seed={wallet.walletAddress ? wallet.walletAddress.toString() : "Orbit MultiSig"}
+                                            size={15}
+                                            scale={3}
+                                        />
+                                        <div className="absolute top-0 -right-2 bg-accent text-black text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                                            {wallet.requiredSignatures}/{wallet.signerAddresses.length}
                                         </div>
                                     </div>
-                                    <div className="flex items-center space-x-3">
-
-                                        <span className="font-bold">BTTC</span>
-                                        <button>
-                                            <MoreVertical className="text-gray-400" />
-                                        </button>
+                                    <div className="pl-4">
+                                        <p className="font-medium">{wallet.name}</p>
+                                        <p className="text-gray-500 font-medium">{wallet.walletAddress}</p>
                                     </div>
                                 </div>
-                                <div className="mt-6 flex items-center text-md text-gray-400">
-                                    <span className="text-gray-400 mr-2">Threshold -</span>
-                                    <span className="text-white mr-2">{wallet.requiredSignatures} <span className="text-gray-500">out of {wallet.signerAddresses.length} signer(s)</span></span>
+                                <div className="flex items-center space-x-3">
+                                <span className="font-bold">BTTC</span>
+                                <span className="text-white ml-2">
+                                {balances[wallet.walletAddress] ? formatUnits(BigInt(balances[wallet.walletAddress]), 18) : "Loading..."}
+                                </span>
+                                <button>
+                                <MoreVertical className="text-gray-400" />
+                                </button>
                                 </div>
-
-                                <div className="mt-6 flex items-center text-md text-gray-400">
-                                    {wallet.createdAt ? <><span className="text-gray-400 mr-4"> Created at -</span><span className="text-white mr-2">
+                            </div>
+                            <div className="mt-6 flex items-center text-md text-gray-400">
+                                <span className="text-gray-400 mr-2">Threshold -</span>
+                                <span className="text-white mr-2">{wallet.requiredSignatures} <span className="text-gray-500">out of {wallet.signerAddresses.length} signer(s)</span></span>
+                            </div>
+                            <div className="mt-6 flex items-center text-md text-gray-400">
+                                {wallet.createdAt ? <>
+                                    <span className="text-gray-400 mr-4"> Created at -</span>
+                                    <span className="text-white mr-2">
                                         {new Date(wallet.createdAt).toLocaleDateString('en-US', {
                                             weekday: 'short',
                                             year: 'numeric',
@@ -100,18 +140,18 @@ function Accounts() {
                                             minute: '2-digit',
                                             hour12: true,
                                         })}
-                                    </span> </> : null}
-                                </div>
-                            </Link>)
-                        ) : (
-                            <p className="text-gray-400 text-center py-8">
-                                You don't have any Orbit Multisig Smart Wallets yet
-                            </p>
-                        )}
-                    </div>
+                                    </span>
+                                </> : null}
+                            </div>
+                        </Link>
+                    )) : (
+                        <p className="text-gray-400 text-center py-8">
+                            You don't have any Orbit Multisig Smart Wallets yet
+                        </p>
+                    )}
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
